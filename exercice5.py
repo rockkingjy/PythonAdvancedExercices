f4f908647e9c89d223b3083dbf8401cd68339562
rockkingjy <rockking.jy@gmail.com>
2016-11-20 17:19:57 +0100

    exe5

--- exercice5.py
+++ exercice5.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt1
+
+import cPickle as pickle
 
 import csv as csv
 import numpy as np
@@ -14,108 +15,133 @@ data = np.array(data) 									# Then convert from a list to an array.
 
 #Question 1
 #print(data[5][4])
-modaliteNan = [0]*len(data[0])
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        if(data[i][j]==""):
-            modaliteNan[j]+=1
-print(modaliteNan)
-#[0, 0, 0, 0, 0, 177, 0, 0, 0, 0, 687, 2]
+def Question1():
+	modaliteNan = [0]*len(data[0])
+	for i in range(len(data)):
+		for j in range(len(data[0])):
+		    if(data[i][j]==""):
+		        modaliteNan[j]+=1
+	print(modaliteNan)
+# Question1()
+# [0, 0, 0, 0, 0, 177, 0, 0, 0, 0, 687, 2]
 
-#Question 3
-agepre = [None]*len(data)
-for i in range(len(data)):
-    if(data[i][5]!=""):
-        agepre[i]=float(data[i][5])
-age = []
-for i in range(len(agepre)):
-    if(agepre[i]!=None):
-           age.append(agepre[i])
-"""
-plt.hist(age)
-plt.title("Titanic Age Histogram")
-plt.xlabel("Age")
-plt.ylabel("Frequency")
-plt.show()
-"""
+#Question 2
+def Question2write():
+	with open('filename', 'wb') as f:
+		dataserial = []
+		for i in range(len(data)): 
+			dataserial.append(data[i][1]) #survived
+		for i in range(len(data)):
+			dataserial.append(data[i][2]) #Pclass
+		for i in range(len(data)):
+			dataserial.append(data[i][4]) #Sex
+		for i in range(len(data)):
+			dataserial.append(data[i][5]) #Age
+		for i in range(len(data)):
+			dataserial.append(data[i][11]) #Embarked
+		pickle.dump(dataserial, f)
+def Question2read():
+    with open('filename','rb') as f:
+        var = pickle.load(f)
+        print var
+#Question2write()
+#Question2read()
+
+#Question 3 
+def Question3(bin = 20):
+	agepre = [None]*len(data)
+	for i in range(len(data)):
+		if(data[i][5]!=""):
+		    agepre[i]=float(data[i][5])
+	age = []
+	for i in range(len(agepre)):
+		if(agepre[i]!=None):
+		       age.append(agepre[i])
+	plt.hist(age,bin)
+	plt.title("Titanic Age Histogram")
+	plt.xlabel("Age")
+	plt.ylabel("Frequency")
+	plt.show()
+#Question3()
+
+#Question 4
+def Question4():
+	Question3(8)
+#Question4()
 
 #Question 5
-#a]
-survecu = [None]*len(data)
-for i in range(len(data)):
-    if(data[i][1]!=""):
-        survecu[i]=float(data[i][1])
-#print(survecu)
-numsur = 0
-for i in range(len(survecu)):
-    if(survecu[i]==1):
-        numsur+=1
-print("a) La proportion de passagers ayant survecu: {0}".format(numsur/float(len(survecu))))
+#a)
+def Question5a():
+	onboat = [None]*len(data)
+	for i in range(len(data)):
+		if(data[i][1]!=""):
+		    onboat[i]=float(data[i][1])
+	numsur = 0
+	for i in range(len(onboat)):
+		if(onboat[i]==1):
+		    numsur+=1
+	print("a) La proportion de passagers ayant survecu: {0}".format(numsur/float(len(onboat))))
+	return numsur,len(onboat)
+#Question5a()
 #b)
-numsurmale = 0
-nummale = 0
-numsurfemale = 0
-numfemale = 0
-for i in range(len(data)):
-    if(data[i][4]=="male"):
-        nummale+=1
-        if(float(data[i][1])==1):
-            numsurmale+=1
-    else:
-        numfemale+=1
-        if(float(data[i][1])==1):
-            numsurfemale+=1
-print("b) La proportion de femmes: {0}, la proportion de hommes: {1}").format(float(numsurfemale)/float(numfemale),float(numsurmale)/float(nummale))
+def Question5b():
+	numsurmale = 0
+	nummale = 0
+	numsurfemale = 0
+	numfemale = 0
+	for i in range(len(data)):
+		if(data[i][4]=="male"):
+		    nummale+=1
+		    if(float(data[i][1])==1):
+		        numsurmale+=1
+		else:
+		    numfemale+=1
+		    if(float(data[i][1])==1):
+		        numsurfemale+=1
+	print("b) La proportion de femmes ayant survecu: {0},\n \
+	la proportion de hommes ayant survecu: {1}").format( \
+	float(numsurfemale)/float(numfemale),float(numsurmale)/float(nummale))
+	return numsurmale, nummale, numsurfemale, numfemale
+#Question5b()
 #c)
-"""
-N = 2
-ind= (0,1)
-plotsur = (len(survecu)-numsur, numsur)
-plotsurmale = (nummale-numsurmale, numsurmale)
-plotsurfemale = (numfemale-numsurfemale, numsurfemale)
-print(plotsurmale,plotsurfemale)
-ind = np.arange(N)    # the x locations for the groups
-width = 0.35       # the width of the bars: can also be len(x) sequence
-#p1 = plt.barh(ind, plotsur, width, color='b')
-plt.title('Survival Breakdown(1=Survived, 0=Died)')
-pos = [0.25,1.25]
-plt.yticks(pos, ('0', '1'))
-#plt.show()
+def Question5c1(numsur, numall):
+	plotsur = (numall-numsur, numsur)
 
-p1 = plt.barh(ind, plotsurmale, width, color='b',alpha=0.8)
-p2 = plt.barh(ind, plotsurfemale, width, color='r',alpha=0.8)
-plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-#plt.show()
-"""
+	fig=plt.figure()
+	ytickspos = [0, 0.5] #position of y tick labels of the bar
+	h = 0.2 #height of the bar
+	b = [0-h/2,0.5-h/2] #bottom of the bar
+	p1 = plt.barh(bottom = b, width = plotsur, height = h, left=None, color='b')
+	plt.title('Survival Breakdown')
+	plt.yticks(ytickspos, ('Died', 'Survived'))
+	plt.grid()
+	plt.show()
+#(numsur, numall) = Question5a()
+#Question5c1(numsur, numall)
+
+def Question5c2(numsurmale, nummale, numsurfemale, numfemale):
+	plotsurmale = (nummale-numsurmale, numsurmale)
+	plotsurfemale = (numfemale-numsurfemale, numsurfemale)
+	
+	fig=plt.figure()
+	ytickspos = [0, 0.5] #position of y tick labels of the bar
+	h = 0.2 #height of the bar
+	b = [0-h/2,0.5-h/2] #bottom of the bar
+	alf = 0.5
+	p1 = plt.barh(b, plotsurmale, h, color='b',alpha=alf)
+	p2 = plt.barh(b, plotsurfemale, h, color='r',alpha=alf)
+	plt.legend((p1[0], p2[0]), ('Men', 'Women'))
+	plt.yticks(ytickspos, ('Died', 'Survived'))
+	plt.grid()
+	plt.show()
+#(numsurmale, nummale, numsurfemale, numfemale) = Question5b()
+#Question5c2(numsurmale, nummale, numsurfemale, numfemale)
 #d)Non
 #e)f)
-def variableSur(variable):
+def Question5ef(variable):
     if(variable=="Sex"):
-        numsurmale = 0
-        nummale = 0
-        numsurfemale = 0
-        numfemale = 0
-        for i in range(len(data)):
-            if(data[i][4]=="male"):
-                nummale+=1
-                if(float(data[i][1])==1):
-                    numsurmale+=1
-            else:
-                numfemale+=1
-                if(float(data[i][1])==1):
-                    numsurfemale+=1
-        N = 2
-        ind= (0,1)
-        plotsurmale = (nummale-numsurmale, numsurmale)
-        plotsurfemale = (numfemale-numsurfemale, numsurfemale)
-        ind = np.arange(N)    # the x locations for the groups
-        width = 0.35       # the width of the bars: can also be len(x) sequence
-        pos = [0.25,1.25]
-        plt.yticks(pos, ('0', '1'))
-        p1 = plt.barh(ind, plotsurmale, width, color='b',alpha=0.8)
-        p2 = plt.barh(ind, plotsurfemale, width, color='r',alpha=0.8)
-        plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-        plt.show()
+        (numsurmale, nummale, numsurfemale, numfemale) = Question5b()
+        Question5c2(numsurmale, nummale, numsurfemale, numfemale)
     elif(variable=="Pclass"):
         num1sur=0
         num1=0
@@ -136,20 +162,19 @@ def variableSur(variable):
                 num3+=1
                 if(float(data[i][1])==1):
                     num3sur+=1
-        N = 2
-        ind= (0,1)
         plot1 = (num1-num1sur, num1sur)
         plot2 = (num2-num2sur, num2sur)
         plot3 = (num3-num3sur, num3sur)
-        print(plot1)
-        ind = np.arange(N)    # the x locations for the groups
-        width = 0.35       # the width of the bars: can also be len(x) sequence
-        pos = [0.25,1.25,2.25]
-        plt.yticks(pos, ('1', '2', '3'))
-        p1 = plt.barh(ind, plot1, width, color='y',alpha=0.5)
-        p2 = plt.barh(ind, plot2, width, color='b',alpha=0.5)
-        p3 = plt.barh(ind, plot3, width, color='r',alpha=0.5)
+
+        ytickspos = [0, 0.5] #position of y tick labels of the bar
+        h = 0.2 #height of the bar
+        b = [0-h/2,0.5-h/2] #bottom of the bar
+        alf = 0.5
+        p1 = plt.barh(b, plot1, h, color='y',alpha=alf)
+        p2 = plt.barh(b, plot2, h, color='b',alpha=alf)
+        p3 = plt.barh(b, plot3, h, color='r',alpha=alf)
         plt.legend((p1[0], p2[0], p3[0]), ('class1', 'class2', 'class3'))
+        plt.yticks(ytickspos, ('Died', 'Survived'))
         plt.show()
     elif(variable=="Age"):
         num1sur=0
@@ -172,20 +197,19 @@ def variableSur(variable):
                     num3+=1
                     if(float(data[i][1])==1):
                         num3sur+=1
-        N = 2
-        ind= (0,1)
         plot1 = (num1-num1sur, num1sur)
         plot2 = (num2-num2sur, num2sur)
         plot3 = (num3-num3sur, num3sur)
-        print(plot1)
-        ind = np.arange(N)    # the x locations for the groups
-        width = 0.35       # the width of the bars: can also be len(x) sequence
-        pos = [0.25,1.25,2.25]
-        plt.yticks(pos, ('1', '2', '3'))
-        p1 = plt.barh(ind, plot1, width, color='y',alpha=0.5)
-        p2 = plt.barh(ind, plot2, width, color='b',alpha=0.5)
-        p3 = plt.barh(ind, plot3, width, color='r',alpha=0.5)
+
+        ytickspos = [0, 0.5] #position of y tick labels of the bar
+        h = 0.2 #height of the bar
+        b = [0-h/2,0.5-h/2] #bottom of the bar
+        alf = 0.5
+        p1 = plt.barh(b, plot1, h, color='y',alpha=alf)
+        p2 = plt.barh(b, plot2, h, color='b',alpha=alf)
+        p3 = plt.barh(b, plot3, h, color='r',alpha=alf)
         plt.legend((p1[0], p2[0], p3[0]), ('<20', '>50', 'other'))
+        plt.yticks(ytickspos, ('Died', 'Survived'))
         plt.show()
     elif(variable=="Embarked"):
         num1sur=0
@@ -207,23 +231,99 @@ def variableSur(variable):
                 num3+=1
                 if(float(data[i][1])==1):
                     num3sur+=1
-        N = 2
-        ind= (0,1)
         plot1 = (num1-num1sur, num1sur)
         plot2 = (num2-num2sur, num2sur)
         plot3 = (num3-num3sur, num3sur)
-        print(plot1)
-        ind = np.arange(N)    # the x locations for the groups
-        width = 0.35       # the width of the bars: can also be len(x) sequence
-        pos = [0.25,1.25,2.25]
-        plt.yticks(pos, ('1', '2', '3'))
-        p1 = plt.barh(ind, plot1, width, color='y',alpha=0.5)
-        p2 = plt.barh(ind, plot2, width, color='b',alpha=0.5)
-        p3 = plt.barh(ind, plot3, width, color='r',alpha=0.5)
+
+        ytickspos = [0, 0.5] #position of y tick labels of the bar
+        h = 0.2 #height of the bar
+        b = [0-h/2,0.5-h/2] #bottom of the bar
+        alf = 0.5
+        p1 = plt.barh(b, plot1, h, color='y',alpha=alf)
+        p2 = plt.barh(b, plot2, h, color='b',alpha=alf)
+        p3 = plt.barh(b, plot3, h, color='r',alpha=alf)
         plt.legend((p1[0], p2[0], p3[0]), ('S', 'C', 'Q'))
+        plt.yticks(ytickspos, ('Died', 'Survived'))
         plt.show()
-        
-#variableSur("Age")
+#Question5ef("Embarked")
 #g)
+import scipy.stats as stats
+import pandas as pd
+def Question5g():
+	df = pd.read_csv('train.csv')
+	df1,df2 = df['Survived'], df['Sex']
+	result = [[sum((df1 == cat1) & (df2 == cat2))
+              for cat1 in (0,1)]
+              for cat2 in ('male','female')]
+	print result
+	chi2,p,dof,expected = stats.chi2_contingency(result)
+	print chi2, p
+#Question5g()
+
+#Question 6,7
+import math
+def Question67(variable='Sex'):
+	df = pd.read_csv('train.csv')
+	df1,df2 = df['Survived'], df[variable]
+	if(variable=='Sex'):
+		items = ('male','female')
+	elif(variable=='Pclass'):
+		items = (1,2,3)
+	elif(variable=='Age'):
+		pd.options.mode.chained_assignment = None # default='warn'
+		for age in range(len(df2)):
+			if math.isnan(df2[age]) != True:
+				if df2[age] < 20:
+					df2[age] = '<20'
+				elif df2[age] > 50:
+					df2[age] = '>50'
+				else:
+					df2[age] = '>20<50'      
+		print df2  
+		items = ('<20','>50','>20<50')
+	elif(variable=='Embarked'):
+		items = ('C','S','Q')
+	result = [[sum((df1 == cat1) & (df2 == cat2))
+              for cat1 in (0,1)]
+              for cat2 in items]
+	print result
+	chi2,p,dof,expected = stats.chi2_contingency(result)
+	print chi2, p
+#Question67('Age')
+
+#Question 8
+import matplotlib as mpl
+def Question8():
+	df = pd.read_csv('train.csv')
+	df1,df2 = df['Age'], df['Pclass']
+	cut = [14,19,22,25,28,31,36,42,50,80]
+
+	xedges = range(10)
+	yedges = [1, 2, 3]
+
+	H = np.ones((3, 10)).cumsum().reshape(3, 10)
+	print(H[::-1])  # This shows the bin content in the order as plotted
+
+	for age in range(len(df1)):
+		for i in range(len(cut)):
+			if(df1[age] < cut[i]):
+				#print i, int(df2[age])-1
+				H[int(df2[age])-1][i] += 1
+				break;
+	print H	
+	fig = plt.figure(figsize=(7, 3))
+	plt.title('Class and age square binning')
+	im = plt.imshow(H, interpolation='nearest', origin='low',
+		            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+	ytickspos = [1, 2, 3] #position of y tick labels of the bar
+	plt.yticks(ytickspos, ('Class1', 'Class2', 'Class3'))
+	plt.xticks(xedges, ('[0,14]', '[14,19]', '[19,22]', '[22,25]', '[25,28]', '[28,31]', '[31,36]', '[36,42]', '[42,50]', '[50,80]'))
+	plt.show()
+Question8()
+
+#Question 9
+
+
+
 
 
